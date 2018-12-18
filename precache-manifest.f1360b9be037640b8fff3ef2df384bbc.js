ad5b0760462ad1b370d90da7960de03677c8d292
sdebrock <sdebrock@Shanes-MacBook-Pro.local>
2018-12-17 19:02:30 -0700

    Updates

---
+++ precache-manifest.f1360b9be037640b8fff3ef2df384bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "284b7e7dc94324df4644",
+    "url": "/react/static/css/main.5b677bc7.chunk.css"
+  },
+  {
+    "revision": "284b7e7dc94324df4644",
+    "url": "/react/static/js/main.284b7e7d.chunk.js"
+  },
+  {
+    "revision": "5bc363a705c279ce0d67",
+    "url": "/react/static/js/1.5bc363a7.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "c6eff1657b8cfcf6c92aa25eb677221a",
+    "url": "/react/index.html"
+  }
+];
